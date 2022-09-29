ba05e126b73081c648efbee07aeaf8f4099c6193
Danilo González <danilo.gonzalezt@bci.cl>
2022-09-29 12:36:55 -0300

    Stacks

---
+++ lib/lambda-processor/index.py
@@ -0,0 +1,3 @@
+def processor_handler(event, context):
+  # Procesar imagen y subir a bucket/done
+  print('hola de nuevo')
---
+++ lib/lambda-uploader/index.py
@@ -0,0 +1,3 @@
+def uploader_handler(event, context):
+  # Subir imagen a bucket/raw desde b64
+  print('hola mundo')
