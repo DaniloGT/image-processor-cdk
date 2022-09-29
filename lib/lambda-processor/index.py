12a155661f11b9a52124f8ecf9324f29a54ca58b
PamelaMunozCaceres <pamelamunozcaceres@gmail.com>
2022-09-29 20:19:41 -0300

    ADD read sns event

--- lib/lambda-processor/index.py
+++ lib/lambda-processor/index.py
@@ -6,6 +6,18 @@ def processor_handler(event, context):
   # Procesar imagen y subir a bucket/done
   print('hola de nuevo')
   print(PIL.__version__)
+
+  # Name of the object
+  s3_event_key = (json.loads(event['Records'][0]['Sns']['Message'])
+                  ["Records"][0]["s3"]["object"]["key"])
+  
+  # Name of the bucket
+  s3_bucket = (json.loads(event['Records'][0]['Sns']['Message'])
+                ["Records"][0]["s3"]["bucket"]["name"])
+
+  print(f's3_event_key: {s3_event_key}')
+  print(f's3_bucket: {s3_bucket}')
+
   response = {
     "statusCode": str(200),
     "body": json.dumps(f'Hello from lambda'),
