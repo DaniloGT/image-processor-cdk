54c69239496974a5d114d194dd1c71677f4de3dd
DaniloGT <dev.dgonzalez@gmail.com>
2022-09-30 08:57:24 -0300

    Test form data

--- lib/lambda-uploader/index.py
+++ lib/lambda-uploader/index.py
@@ -1,11 +1,22 @@
 import json
+import os
+import boto3
 
 def uploader_handler(event, context):
   # Subir imagen a bucket/raw desde b64
 
+  s3 = boto3.resource("s3")
+
+  bucket_name = os.environ['BUCKET_NAME']
+
+  s3_path = 'raw/file.txt'
+  event_p = json.dumps(event)
+
+  s3.Bucket(bucket_name).put_object(Key=s3_path, Body=event_p.encode('utf-8'))
+
   response = {
     "statusCode": str(200),
-    "body": json.dumps(f'Hello from lambda'),
+    "body": json.dumps(f'File saved'),
     "headers": {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": '*',
