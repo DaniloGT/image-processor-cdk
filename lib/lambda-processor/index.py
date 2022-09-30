d8ba1883f0e25e510680549de8e35796ccb94a3e
PamelaMunozCaceres <pamelamunozcaceres@gmail.com>
2022-09-30 09:09:02 -0300

    UPDATE response headers

--- lib/lambda-processor/index.py
+++ lib/lambda-processor/index.py
@@ -34,13 +34,6 @@ def processor_handler(event, context):
   response = {
     "statusCode": str(200),
     "body": json.dumps(f'Hello from lambda'),
-    "headers": {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": '*',
-      "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
-      "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,X-Amz-Security-Token,Authorization,X-Api-Key,X-Requested-With,Accept,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Access-Control-Allow-Headers",
-      "X-Requested-With": "*"
-    },
   }
 
   return response
