0fec1a249c85cd42d171408cf0b906a17f526f19
DaniloGT <dev.dgonzalez@gmail.com>
2022-09-30 15:29:40 -0300

    Dockerfile and layer

--- lib/layer.cmd
+++ lib/layer.cmd
@@ -1,1 +1,14 @@
-pip3 install -r lambda-processor/requirements.txt --target ./lambda-processor-layer/python/lib/python3.9/site-packages
+REM Build image
+docker build . -t libs:latest
+
+REM Run image and assign id to variable
+docker run -d -i libs:latest > tmpFile
+set /p containerId= < tmpFile
+del tmpFile
+
+REM Copy libs from docker to host
+docker cp %containerId%:var/task/lambda-processor-layer ./lambda-processor-layer
+
+REM Kill image
+docker kill %containerId%
+docker rm %containerId%
