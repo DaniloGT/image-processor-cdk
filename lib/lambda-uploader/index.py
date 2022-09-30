03bfa8e81528daf68e9a90fac48814e1ddfebecd
DaniloGT <dev.dgonzalez@gmail.com>
2022-09-30 10:12:10 -0300

    Add content type

--- lib/lambda-uploader/index.py
+++ lib/lambda-uploader/index.py
@@ -10,14 +10,15 @@ def uploader_handler(event, context):
   """
 
   image_name = str(uuid.uuid4())
+  content_type = event["headers"]["Content-Type"].split("/")[-1]
   s3 = boto3.client("s3")
-  bucket_name = os.environ['BUCKET_NAME']
+  bucket_name = os.environ["BUCKET_NAME"]
 
   b_file = base64.b64decode(event["body"])
 
   s3.put_object(
     Bucket = bucket_name,
-    Key = image_name,
+    Key = f"raw/{image_name}.{content_type}",
     Body=b_file
   )
 
@@ -25,7 +26,7 @@ def uploader_handler(event, context):
     "statusCode": str(200),
     "body": json.dumps({
       "message": "Your image was correctly uploaded",
-      "url": "wena/pagina"
+      "url": f"wena/pagina/{image_name}"
     }),
     "headers": {
       "Content-Type": "application/json",
